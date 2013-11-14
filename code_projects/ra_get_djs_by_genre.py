7708edff851f1cffeedb59455268c26fd322d054
John Dingee <dingeej@gmail.com>
2013-11-14 00:29:29 -0500

    clean up ra parser

--- code_projects/ra_get_djs_by_genre.py
+++ code_projects/ra_get_djs_by_genre.py
@@ -1,48 +1,35 @@
 from HTMLParser import HTMLParser
 import urllib2
+import sys
 
-##http://www.residentadvisor.net/dj.aspx?style=20 #deephouse
-##http://www.residentadvisor.net/dj.aspx?style=5 #tech
+djs = []
+genre_iterator = [{'DeepHouse':20}
+,{'Techno':5}
+,{'Tech House':15}]
 
-
-#def get_genres(self):
-	## look at the drop down menu in the source here:view-source:http://www.residentadvisor.net/dj.aspx#A
-
-def bla():
-	genre_iterator = [{'DeepHouse':20}
-	,{'Techno':5}
-	,{'Tech House':15}]
-
-	for x in genre_iterator:
-		for k,v in x.items():
-			print 'key: '  + str(k) + ' val: '  + str(v)
-		#print 'value' + v
-
-# create a subclass and override the handler methods
-
-'''
 class MyHTMLParser(HTMLParser):
 	def handle_starttag(self, tag, attrs):
-	  print "Encountered a start tag:", tag
+		a = 1
 	def handle_endtag(self, tag):
-	  print "Encountered an end tag :", tag
+	  b = 2
 	def handle_data(self, data):
 	  print "Encountered some data  :", data
 	  djs.append(data)
 
-def parse_top_1000(url):
+def get_html(url):
 	response = urllib2.urlopen(url)
 	html = response.read()
-	#print html
-
-	start_str = ''
-	end_str = '' 
-
-	# instantiate the parser and fed it some HTML
 	parser = MyHTMLParser()
 	parser.feed(html)
-	print str(djs)
-'''
+
+def get_artists():
+	for x in genre_iterator:
+		for genre,genre_id in x.items():
+			print 'genre: '  + str(genre) + ' genre_id: '  + str(genre_id)
+			request = 'http://www.residentadvisor.net/dj.aspx?style=' + str(genre_id)
+			genre_source_html = get_html(request)
+
 if __name__ == '__main__':
-	#top_1000 = 'http://www.residentadvisor.net/dj.aspx'
-	bla()
+	get_artists()
+	print djs
+
