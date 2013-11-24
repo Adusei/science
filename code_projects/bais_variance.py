d3c646f45f80812accfed6836f8914268ba111c8
John Dingee <dingeej@gmail.com>
2013-11-24 17:16:41 -0500

    get started on bais variance hw

---
+++ code_projects/bais_variance.py
@@ -0,0 +1,76 @@
+#!/usr/bin/env python
+import math as m
+from random import choice
+
+def mse(y,h):
+    return(np.mean(np.square(y-h)))
+
+# Consider a case where target function is  sin( pi x ) 
+def target_fn(x):
+	x = float(x)
+	y = m.sin(x)
+	return y
+
+def model_1 (ex_1,ex_2):
+	# minimize mean squared error.
+	# a(x) + b
+	y_1 = target_fn(ex_1)
+	y_2 = target_fn(ex_2)
+
+	print 'y_1:' + str(y_1)
+	print 'y_2:' + str(y_2)
+	return y_1, y_2
+
+	#return a, b
+
+#def model_2 (x):
+	# b = x
+	# return b
+
+def generate_distribution(range_len):
+# the input distribution is uniform on [ -1, 1 ]
+	nmbs =list(range(1,range_len))
+	distr_x = []
+	distr_y = []
+
+	for nm in nmbs:
+		distr_x.append(float(nm / float(range_len)))
+		distr_x.append(float(-(nm / float(range_len))))
+
+	return distr_x
+
+def generate_sample():
+	distr_x = generate_distribution(100)
+	#for x_instance in distr_x:
+		#y_instance = target_fn(x_instance)
+		#distr_y.append(y_instance)
+	
+	sample_1 = choice(distr_x)
+	sample_2 = choice(distr_x)
+
+	#print sample_1
+	return sample_1,sample_2
+
+	#print str(distr_y)
+
+
+if __name__ == '__main__':
+  #x_range = generate_distribution(100)
+  foo,bar = generate_sample()
+  e,r = model_1(foo,bar)
+  print foo
+  print bar
+  print e 
+  print r
+  #print x_range
+
+
+# training set has two examples and the learning
+# algorithm tries to minize the mean squared error.
+
+# Assume that we have two learning models
+# consisting of all hypothesis in the form of:
+
+# h(x) = ax + b
+
+# h(x) = b
--- code_projects/wine_knn.py
+++ code_projects/wine_knn.py
@@ -11,14 +11,17 @@ from sklearn.datasets import load_iris
 from sklearn.metrics import confusion_matrix
 from sklearn.neighbors import KNeighborsClassifier as knn
 from sklearn.preprocessing import scale
+from sklearn.linear_model import LogisticRegression as LR
+
 
 data_url = 'https://raw.github.com/dzorlu/GADS/4c3ee79773d3c6024979e20d337ad2210be68ac9/data/wine.txt'
 
 NUM_NBRS = 21
-FOLDS = 10
+FOLDS = 20
 RESULTS = []
 CROSS_VAL_ARRAY = []
-
+LOG_REG_ARRAY = []
+VIZ_ARRAY = []
 
 def process_data(data_src):
   header = ['label','Alcohol','Malic acid','Ash','Alcalinity of ash','Magnesium','Total phenols','Flavanoids','Nonflavanoid phenols','Proanthocyanins','Color intensity','Hue','OD280/OD315 of diluted wines','Proline']
@@ -28,10 +31,11 @@ def process_data(data_src):
   labels = data.label 
 
   #run_knn(features, labels, features,labels,1,5)
+  run_log_reg(features, labels)
   #cross_val( features, labels, FOLDS ,NUM_NBRS)
-  find_opt_k ( features, labels )
+  #find_opt_k ( features, labels )
 
-def cross_val(features, labels, num_folds, k):
+def cross_val(features, labels, num_folds, k=None, algo='knn'):
     cv_results = {}
     kf = cv.KFold(n=len(features), n_folds=num_folds, shuffle=True)
 
@@ -48,17 +52,19 @@ def cross_val(features, labels, num_folds, k):
 
     cv_results['k'] = k
     cv_results['avg_acc'] = avg_acc
+    cv_results['algo'] = algo
     RESULTS.append(cv_results)
+    VIZ_ARRAY.append(1 - avg_acc)
     print cv_results
 
 def find_opt_k(features,labels):
     CROSS_VAL_ARRAY = []
-    nmbs =list(range(1,101))
+    nmbs =list(range(1,51))
     for nm in nmbs:
       if nm % 2 != 0:
         loop_ind = '\n'+ '=' * 20 + 'k: ' + str(nm) + '=' * 20 
         print loop_ind * 3 + '\n'
-        cross_val (features, labels, FOLDS, nm)
+        cross_val (features, labels, FOLDS, nm,'knn')
 
 def run_knn(train_features, train_labels,test_features, test_labels, iteration=None,k=None):
     clf = knn(n_neighbors = k)
@@ -69,17 +75,39 @@ def run_knn(train_features, train_labels,test_features, test_labels, iteration=N
     predicted_labels = clf.predict(test_features)
     cm = confusion_matrix(test_labels, predicted_labels)
 
-    #print 'iteration:'.format(iteration)
-    #print 'num_features = {0}'.format(num_features)
-    #print 'k = {0}'.format(k)
-    #print 'accuracy = {0} %\n'.format(round(100 * acc, 2))
-    #print 'confusion matrix:\n', cm, '\n'
-
     CROSS_VAL_ARRAY.append(acc)
+
+def kfolds(features, labels,num_folds):
+    kf = cv.KFold(n=len(features), n_folds=num_folds, shuffle=True)
+    return kf
+
+def run_log_reg (features, labels):
+    #model_results = {}
+    #num_recs = len(data)
+    kf = kfolds(features, labels, FOLDS)
+
+    for i, (train_index, test_index) in enumerate(kf):
+      model = LR()
+
+      train_features = features.loc[train_index].dropna()
+      train_labels = labels.loc[train_index].dropna()
+
+      test_features = features.loc[test_index].dropna()
+      test_labels = labels.loc[test_index].dropna()
+
+      model.fit(train_features, train_labels)
+      # get model outputs
+      inputs = map(str, train_features.columns.format())
+      coeffs = model.coef_[0]
+      accuracy = model.score(test_features, test_labels)
+
+      print 'accuracy:' + str(accuracy)
 
 if __name__ == '__main__':
   process_data(data_url)
   pp.pprint(RESULTS)
+  #print '\n\n'
+  #print VIZ_ARRAY
 
 #http://archive.ics.uci.edu/ml/datasets/Wine
 #i) Fit a kNN algorithm. Perform cross-validation to pick the best value for k.
@@ -87,7 +115,7 @@ if __name__ == '__main__':
     # how do i balance a high accuracy for a low 
     # value of k given that when k approaches zero
     # the model becomes over fit?
-    
+
 #ii) Perform cross-validation to pick the best model between logistic regression and kNN. Does the answer change when you use different CV technique?
 
 
