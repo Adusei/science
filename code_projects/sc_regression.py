9030123fe1fd14122a014214ecdaf73b804cf730
John Dingee <dingeej@gmail.com>
2013-11-29 23:36:03 -0500

    try/except for play count and favorite count

--- code_projects/sc_regression.py
+++ code_projects/sc_regression.py
@@ -3,6 +3,8 @@ import pprint as pp
 import pandas as pd
 import numpy as np
 import random as rd
+import datetime as dt
+import json
 
 
 from sklearn.linear_model import LogisticRegression as LR
@@ -16,26 +18,48 @@ client = sc.Client(client_id='7460ba6c0c64e2e019aeca796eb3a4f7'
                   ,username='Adusei'
                   ,password='Dinginator06')
 
+def create_track_dict(sc_track_object):
+  track_dict = {}
+  track_dict['track_id']= sc_track_object.id
+  track_dict['title']= sc_track_object.title
+  track_dict['tags'] = sc_track_object.tag_list
+  track_dict['duration']= ( sc_track_object.duration ) / 60000.00 #minutes
+
+  #a = dt.datetime(sc_track_object.created_at)
+  #b = dt.datetime(2013,12,31,23,59,59)
+  #(b-a).total_seconds()
+
+
+  try:
+     fc = sc_track_object.favoritings_count
+  except AttributeError:
+     fc = -1
+
+  track_dict['favs_count'] = fc
+
+  try:
+     pc = sc_track_object.favoritings_count
+  except AttributeError:
+     pc = -1
+
+  track_dict['play_count'] = pc
+  
+  return track_dict
+
 def get_my_favs():
+  #http://api.soundcloud.com/tracks/92518013.json?client_id=7460ba6c0c64e2e019aeca796eb3a4f7
   all_of_my_favs = []
 
   my_id = client.get('/me').id
-  
-  my_favorites_request = 'users/'+ str(my_id) +'/favorites'
+  response = 'users/'+ str(my_id) +'/favorites'
+  my_favorites = client.get(response)
 
-  my_favorites = client.get(my_favorites_request)
-
-  for my_fav_instance in my_favorites:
-      favs_dict = {}
-      favs_dict['track_id']= my_fav_instance.id
-      favs_dict['title']= my_fav_instance.title
-      #favs_dict['random_number']= rd.uniform(0,1)
-      #favs_dict['playback_count'] = my_fav_instance.playback_count
-      favs_dict['duration']= ( my_fav_instance.duration ) / 60000.00 #minutes
-      all_of_my_favs.append(favs_dict)
-      pp.pprint
+  for f in my_favorites:
+    favs_dict = create_track_dict(f)
+    all_of_my_favs.append(favs_dict)
 
   return all_of_my_favs
+
 
 def get_random_tracks(limit_input):
   all_random_tracks = []
@@ -43,15 +67,8 @@ def get_random_tracks(limit_input):
   tracks = client.get('/tracks', limit=limit_input)
 
   for track in tracks: # make this modular...
-    tracks_dict = {}
-    tracks_dict['track_id']= track.id
-    tracks_dict['title']= track.title
-    #tracks_dict['random_number']= rd.uniform(0,1)
-    #tracks_dict['playback_count'] = track.playback_count
-    tracks_dict['duration']= ( track.duration ) / 60000.00 #minutes
+    tracks_dict = create_track_dict(track)
     all_random_tracks.append(tracks_dict)
-
-  #pp.pprint(all_random_tracks)
 
   return all_random_tracks
 
@@ -84,9 +101,9 @@ def kfolds(features, labels, num_folds):
     kf = cv.KFold(n=len(features), n_folds=num_folds, shuffle=True ,random_state=0, indices=False)
     return kf
 
-def run_logistic_regression(train_x, train_y, test_x, test_y, iteration):#,iteration):
-  model = LR()                        # model is an "instance" of the class LR
-  model.fit(train_x, train_y)         # perform model fit ("in place")
+def run_logistic_regression(train_x, train_y, test_x, test_y, iteration):
+  model = LR()
+  model.fit(train_x, train_y)
 
    # get model outputs
   inputs = map(str, train_x.columns.format())
@@ -104,6 +121,9 @@ def run_logistic_regression(train_x, train_y, test_x, test_y, iteration):#,itera
 
 def cross_validate(data,num_folds=10):
     data = data.drop('title',1)
+    data = data.drop('tags',1)
+
+
     features = data.drop('label', 1)
     labels = data.label
 
@@ -126,9 +146,11 @@ def cross_validate(data,num_folds=10):
 
       run_logistic_regression(train_features, train_labels, test_features, test_labels, i)
 
+
 if __name__ == '__main__':
   expr  =  get_random_tracks(180)
   cntrl =  get_my_favs()
+  #get_my_favs()
   tracks =  prepare_data(cntrl, expr)
   cross_validate(tracks)
 
