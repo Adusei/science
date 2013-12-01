831b50028c583d03429b7d735f28b226f69546a2
John Dingee <dingeej@gmail.com>
2013-12-01 17:25:01 -0500

    cross validate nltk naive bayes

--- code_projects/naive_bayes.py
+++ code_projects/naive_bayes.py
@@ -2,16 +2,20 @@ import pandas as pd
 import random as rd
 import string as st
 import pprint as pp
+from sklearn import cross_validation as cv
+
 
 import nltk.classify
 from nltk import NaiveBayesClassifier
 
 
+
 url_prefix = 'https://raw.github.com/dzorlu/GADS/master/data/nb_train.csv'
-TRAIN_PCT = 0.7
+NUM_FOLDS = 10
 
 TRAIN = '/Users/john/data/gads/nb_train.csv'
 TEST = '/Users/john/data/gads/nb_test.csv'
+
 
 def process_data(): # FIX THIS!
     all_insults = list()
@@ -32,20 +36,15 @@ def process_data(): # FIX THIS!
     
     return all_insults
 
+
 def nltk_featurize(phrase):
     phrase_features = {}
+    # hfag = 0
 
-    hfuck = 0
-    hfag = 0
-
-    if 'fuck' in phrase.lower():
-        hfuck = 1
-
-    if 'fag' in phrase.lower():
-        hfag = 1
+    # if 'fag' in phrase.lower():
+    #     hfag = 1
      
-    phrase_features['has_fag'] = hfag
-    phrase_features['has_fuck'] = hfuck
+    # phrase_features['has_fag'] = hfag
 
     ###          ###
     ##  TOKENIZE  ##
@@ -60,6 +59,8 @@ def nltk_featurize(phrase):
         phrase_features['']= (token,cnt)
 
     return phrase_features
+
+
     # {'has_fag': 0, 'has_fuck': 1, 'word': ('hello',2) }
 
     ### USE CODE BELOW FOR N-GRAMS ###
@@ -69,16 +70,14 @@ def nltk_featurize(phrase):
     # for k,v in bg_fd.items():
     #     print k,v
 
-def nltk_model(train_comments):
+# def nltk_model(train_comments):
+def nltk_model(train_data, test_data, iteration):
     # assert len(train_comments) == 1000 #?
 
-    rd.shuffle(train_comments)
+    test_set = [(nltk_featurize(comment), is_insult) for comment, is_insult in test_data]
+    train_set = [(nltk_featurize(comment), is_insult) for comment, is_insult in train_data]    
 
-    features = [(nltk_featurize(comment), is_insult) for comment, is_insult in train_comments]
-
-    split_pt = int(TRAIN_PCT * len(features))
-
-    train_set, test_set = features[:split_pt],features[split_pt:]
+    #train_set, test_set = features[:split_pt],features[split_pt:]
     
     nb = NaiveBayesClassifier.train(train_set)
 
@@ -90,35 +89,51 @@ def nltk_model(train_comments):
     
     print 'accuracy = {0} %'.format(int(100 * nltk.classify.accuracy(nb, test_set)))
 
-# def cv (data,excluded_feature=None):
-# # create cv iterator (note: train pct is set implicitly by number of folds)
-#     features = data.drop('label', 1)
-#     labels = data.label 
+def kfolds(features, labels,num_folds):
+    kf = cv.KFold(n=len(features), n_folds=num_folds, shuffle=True)
+    return kf
 
-#     num_recs = len(data)
-#     kf = kfolds(features, labels,NUM_FOLDS)
+def cross_validate (data,excluded_feature=None):
+# create cv iterator (note: train pct is set implicitly by number of folds)
 
-#     for i, (train_index, test_index) in enumerate(kf):
+    features = []
+    labels = []
 
-#             loop_ind = '\n'+ '=' * 20 + 'Loop Number: ' + str(i) + '=' * 20 
+    for feature, label in data:
+        features.append(feature)
+        labels.append(label)
 
-#             print loop_ind * 3 + '\n'
+    num_recs = len(data)
+    kf = kfolds(features, labels,NUM_FOLDS)
 
-#             train_features = features.loc[train_index].dropna()
-#             train_labels = labels.loc[train_index].dropna()
+    for i, (train_index, test_index) in enumerate(kf):
 
-#             test_features = features.loc[test_index].dropna()
-#             test_labels = labels.loc[test_index].dropna()
+        loop_ind = '\n'+ '=' * 20 + 'Loop Number: ' + str(i) + '=' * 20 
 
-#             nltk_model(train_features, train_labels,test_features, test_labels, i, excluded_feature)
+        print loop_ind * 3 + '\n'
 
+        train_data = []
+        test_data = []
+
+        for t in train_index:
+            ftr = features[t]
+            lbl = labels[t]
+            train_data.append((ftr,lbl))
+
+        for t in test_index:
+            ftr = features[t]
+            lbl = labels[t]
+            test_data.append((ftr,lbl))
+            
+        nltk_model(train_data, test_data, i)
 
 if __name__ == '__main__':
 	# train = process_data()
 	# nltk_model()
     #bla = nltk_featurize('hello hello this is john bla bla bla')
     train = process_data()
-    nltk_model (train)
+    # nltk_model (train)
+    cross_validate (train)
 
 ## IDEAS ## 
 
