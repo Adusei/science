764397090c5addf60c20d8f40221708279707af4
John Dingee <dingeej@gmail.com>
2013-12-07 15:12:30 -0500

    get frequency distribution of favorites of users i follow

--- code_projects/sc/recommender_test.py
+++ code_projects/sc/recommender_test.py
@@ -49,73 +49,81 @@ def create_track_dict(sc_track_object):
   return track_dict
 
 def get_favs_by_user(user_id):
-	all_favs = []
+  all_favs = []
 
-	response = 'users/'+ str(user_id) +'/favorites'
-	my_favorites = client.get(response,limit=1000)
+  response = 'users/'+ str(user_id) +'/favorites'
+  my_favorites = client.get(response,limit=1000)
 
-	for f in my_favorites:
-		favs_dict = create_track_dict(f)
-		all_favs.append(favs_dict)
+  for f in my_favorites:
+    favs_dict = create_track_dict(f)
+    all_favs.append(favs_dict)
 
-	return all_favs
+  return all_favs
 
 def get_ngams_by_user (user_id):
 
-	user_ngram_fdist = cl.defaultdict(int)
-	my_favs = get_favs_by_user(user_id)
+  user_ngram_fdist = cl.defaultdict(int)
+  my_favs = get_favs_by_user(user_id)
 
-	for fav in my_favs:
-		title =  fav['title']
-		tag_list = fav['tags']
-		user_id = fav['user_id'] # Not sure how i want to deal with this
+  for fav in my_favs:
+    title =  fav['title']
+    tag_list = fav['tags']
+    user_id = fav['user_id'] # Not sure how i want to deal with this
 
-		tag_list = tag_list.replace('"', '').lower()
-		title = title.lower()
+    tag_list = tag_list.replace('"', '').lower()
+    title = title.lower()
 
-		print title
-		print user_id
+    title_tokens = nltk.word_tokenize(title)
+    tag_tokens = nltk.word_tokenize(tag_list)
 
-		title_tokens = nltk.word_tokenize(title)
-		tag_tokens = nltk.word_tokenize(tag_list)
+    tmp_tokens = title_tokens + tag_tokens
 
-		tmp_tokens = title_tokens + tag_tokens
+    bgs = nltk.bigrams(tmp_tokens)
+    bg_fd = nltk.FreqDist(bgs)
+    for ngram,freq in bg_fd.items():  
+      ngram_str = ngram[0].encode('utf-8') + ' ' + ngram[1].encode('utf-8')
+      user_ngram_fdist[ngram_str] += 1
+      
+  sorted_ngrams = sorted(user_ngram_fdist.iteritems(), key=op.itemgetter(1))  
 
-		bgs = nltk.bigrams(tmp_tokens)
-		bg_fd = nltk.FreqDist(bgs)
-		for ngram,freq in bg_fd.items():	
-			ngram_str = ngram[0].encode('utf-8') + ' ' + ngram[1].encode('utf-8')
-			user_ngram_fdist[ngram_str] += 1
-			
-	sorted_ngrams = sorted(user_ngram_fdist.iteritems(), key=op.itemgetter(1))	
-	pp.pprint(sorted_ngrams)
-
-	return sorted_ngrams
+  return sorted_ngrams
 
 def find_follower_likes(user_id):
-	all_tracks = []
+  all_tracks = cl.defaultdict(int)
 
+  favorites  = 'users/'+ str(user_id) +'/favorites'
+  user_favorites = client.get(favorites,limit=1000)
 
-	response = 'users/'+ str(user_id) +'/followings' #followers
-	my_favorites = client.get(response,limit=1000)
+  response = 'users/'+ str(user_id) +'/followings'
+  user_followings = client.get(response,limit=1000)
 
-	for f in my_favorites:
-		if f.duration > 3600000.0: #1hr
-			favs_dict = create_track_dict(f)
-			all_tracks.append(favs_dict)
-			user_favs = get_favs_by_user(user_id)
+  for f in user_followings:
+    print 'USER:  ' + f.username
+    followed_user_id = f.id
+    user_favs = get_favs_by_user(followed_user_id)
+    for track in user_favs:
+      if track['duration'] > 60.0:
+        track_tuple = (track['track_id'],track['title'])
+        all_tracks[track_tuple] += 1
+        print track_tuple
 
+  # remove favorites that current user also has as a favorite
 
-	return all_tracks
+  sorted_tracks = sorted(all_tracks.iteritems(), key=op.itemgetter(1))  
 
+  pp.pprint(sorted_tracks)
+  return sorted_tracks
 
 if __name__ == "__main__":
-	get_ngams_by_user(6596434) #md
-		# tj 3105250
-		# yoshi 12772525
+  # get_ngams_by_user(6596434) #me
+  find_follower_likes(6596434)
+    # tj 3105250
+    # yoshi 12772525
 
-	# http://api.soundcloud.com/resolve.json?url=https://soundcloud.com/dteej&client_id=7460ba6c0c64e2e019aeca796eb3a4f7 yyy84
-
+  # http://api.soundcloud.com/resolv
+  # e.json?url=https://soundcloud.co
+  # m/dteej&client_id=7460ba6c0c64e2
+  # e019aeca796eb3a4f7
 
 
 
@@ -123,12 +131,3 @@ if __name__ == "__main__":
 # in the way that "better" users
 # titlt the graph in their favor
  
-# Personas... Deep House/Techno/Burning Man/EDM
-
-
-
-
-
-
-
-
