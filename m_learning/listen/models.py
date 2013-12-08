52456058eaaf50e11737fe1fba1ea38e652a93bf
John Dingee <dingeej@gmail.com>
2013-12-08 14:06:54 -0500

    nina kravis on the homepage

--- m_learning/listen/models.py
+++ m_learning/listen/models.py
@@ -1,9 +1,14 @@
 from django.db import models
 
 class Sound(models.Model):
-    title = models.CharField(max_length=100, unique=True,)
-    sc_id = models.IntegerField(max_length=100, unique=True,)
+    title = models.CharField(max_length=100, unique=True)
+    sc_id = models.IntegerField(max_length=100, unique=True)
+    duration_mins = models.DecimalField(decimal_places=5,max_digits=5)
+    jd_following_fav_count = models.IntegerField()
+    ranker = models.DecimalField(decimal_places=5,max_digits=5)
 
     def __unicode__(self):
         # return '%s' % self.name
         return self
+
+
--- m_learning/listen/views.py
+++ m_learning/listen/views.py
@@ -1,10 +1,6 @@
 from django.shortcuts import render,render_to_response
 from listen.models import Sound
 
-
-
-# Create your views here.
-
 def basic(request):
 	return render_to_response("index.html", request)#, {"sound": Sound.objects.all()})
 
