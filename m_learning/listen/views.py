b9995c6a8d0fa2fe4d21ca027be7271a76c0a1d3
John Dingee <dingeej@gmail.com>
2013-12-08 18:04:40 -0500

    render soundcloud iframe with random sound

--- m_learning/listen/views.py
+++ m_learning/listen/views.py
@@ -6,15 +6,12 @@ from django_tables2   import RequestConfig
 from django.template import RequestContext
 from django.http import HttpResponse
 
-def basic (request): 
-	# sound_obj = Sound.objects.all()
-	# return render_to_response("index.html", request, {"sound": Sound.objects.order_by('?')[0]})
-	# return render_to_response("index.html"
-	# 			, request
-	# 			, {"sound": Sound.objects.all()}
-	# 			, context_instance=RequestContext(request))#[0]})
 
-  sound = get_object_or_404(Sound, pk=4)
+def basic (request): 
+  rand_pk = Sound.objects.order_by('?')[0].id
+  sound = get_object_or_404(Sound, pk=rand_pk)
+
+  # iframe = 'sometahing'+ sound.id
   return render(request, 'index.html', {'sound': sound})
 
 
