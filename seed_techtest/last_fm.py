2694096673d1ae98ceb25c4381bac43b667fb8c6
John Dingee <dingeej@gmail.com>
2014-05-06 21:09:13 -0400

    abstract api call

--- seed_techtest/last_fm.py
+++ seed_techtest/last_fm.py
@@ -10,6 +10,18 @@ class LastFM:
         self.API_KEY = "834ca7240545d8fa587d43662c252773"
         # self.SECRET =  "44195d2012dd899f51af02c7861aad21" 
 
+    def api_request(self, url):
+				#Send Request and Collect it
+				data = urllib2.urlopen( url )
+				json_response = json.load ( data )
+
+				data.close()
+				return json_response
+
+
+    # def get_tags_by_artist (self, artist):
+    # 		url = "http://ws.audioscrobbler.com/2.0/?method=artist.getTags&artist=Red%20Hot%20Chili%20Peppers&user=RJ&api_key=834ca7240545d8fa587d43662c252773&format=json"
+
     def get_genre(self, genre, **kwargs):
         kwargs.update({
             "method":	"tag.gettopartists",
@@ -21,15 +33,13 @@ class LastFM:
         try:
 						#Create an API Request
 						url = self.API_URL + "?" + urllib.urlencode(kwargs)
-						#Send Request and Collect it
-						data = urllib2.urlopen( url )
-						#Print it
-						response_data = json.load( data )
+
+						response_data = self.api_request(url)
+
 						artists =  response_data['topartists']['artist']
 						for artist in artists:
 						    print artist['name'].encode('utf8')
 						#Close connection
-						data.close()
         except urllib2.HTTPError, e:
             print "HTTP error: %d" % e.code
         except urllib2.URLError, e:
