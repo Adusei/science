a17fee18dad2ad482a858aa173355797efd2fa88
John Dingee <dingeej@gmail.com>
2014-05-11 00:27:10 -0400

    comments in db module.. abstract table objects

--- last_fm/db.py
+++ last_fm/db.py
@@ -13,80 +13,102 @@ import sqlalchemy as al
 import pprint as pp
 
 class DbTask(object):
-    # ensures the connection the the database is closed on task completion
+
+    # connect to local host
     db_connection_string  = 'mysql://root@localhost/last_fm'
 
+    # create SQL Achemy 
     engine = create_engine(db_connection_string, convert_unicode=True,pool_recycle=3600, pool_size=20)
+
+    # dont tell me everything thats going on with the database
     engine.echo = False
+
+    # create session object
     db_session = scoped_session(sessionmaker(autocommit=True, autoflush=True, bind=engine))
 
+    # define the metadata for my database ( need this to create table objects below )
     metadata = al.MetaData(engine)
+
+    # create the sql alchemy table objects
+    tags_table = al.Table('tags', metadata, autoload=True)
+    artist_table = al.Table('artists', metadata, autoload=True)
+    artist_to_tags_table = al.Table('artist_to_tags', metadata, autoload=True)
 
     def __init__(self):
         print("Initializing DBTask!")
 
     def add_artist (self, artist_name):
-        artist_table = al.Table('artists', self.metadata, autoload=True)
+        # decode the artist name
         artist_name = artist_name.decode('utf8')
         try:
-            artist_ins = artist_table.insert()
+            # prepare insert statement
+            artist_ins = self.artist_table.insert()
+
+            #execute insert statement with provided param
             self.engine.execute(artist_ins,artist_name = artist_name)
-            print '...success fully insertest tag!!' + artist_name
+            print 'success fully insertest tag: ' + artist_name
+
+        # since there is a unique index on artist name the above code 
+        # will only suceed when the artist_name does not exist in the artist table.
         except IntegrityError:
             pass
 
+        # close the database connection
         self.db_session.close()
-        # http://stackoverflow.com/questions/8585346/get-last-inserted-value-from-mysql-using-sqlalchemy    
-        ## DO THIS BEFORE HANDING IN THE CODE... WHAT IS BELOW IS GARBAGE ##
+
 
     def add_tag (self, tag_name):
-        tags_table = al.Table('tags', self.metadata, autoload=True)
+        # decode the tag
         tag_name = tag_name.decode('utf8')
         try:
-            tag_ins = tags_table.insert()
+            #prepare the insert statement and execute with the provided tag name
+            tag_ins = self.tags_table.insert()
             self.engine.execute(tag_ins,tag_name = tag_name)
             print '...success fully insertest tag ' + tag_name
+        
+        # only insert new tags... if tag exists above code will not run
         except IntegrityError:
             pass
 
+        # close db connection
         self.db_session.close()
 
     def add_artist_to_tag (self, artist_id, tag_id, tag_count):
-        artist_to_tags_table = al.Table('artist_to_tags', self.metadata, autoload=True)
-
         try:
-            a_t_ins = artist_to_tags_table.insert()
+            # insert artist to tags based on provided params
+            a_t_ins = self.artist_to_tags_table.insert()
             self.engine.execute(a_t_ins, artist_id = artist_id, tag_id = tag_id, tag_count = tag_count)
+
+            # only exec above code if relation doesnt exists.. artist_id/tag_id is primary key.
         except IntegrityError:
             pass
 
+        # close db connection
         self.db_session.close()
 
-
     def get_tag_by_name (self, tag_name):
-        tags_table = al.Table('tags', self.metadata, autoload=True)
         tag_name = tag_name.decode('utf8') 
 
+        # select from the database where the tag name is what weve passed in
         where_clause = 'tag_name = "' + tag_name + '"'
-        s = tags_table.select().where(where_clause)
+        s = self.tags_table.select().where(where_clause)
 
+        # execute the select and store row in rs
         rs = s.execute()
 
+        # save the tag id and return it
         for r in rs:
             tag_id = r.tag_id
-            print tag_id
 
         self.db_session.close()
         return tag_id
 
 
     def get_artist_by_name(self, artist_name):
-        artist_table = al.Table('artists', self.metadata, autoload=True)
         artist_name = artist_name.decode('utf8') 
 
-
         where_clause = 'artist_name = "' + artist_name + '"'
-        s = artist_table.select().where(where_clause) #.limit(1)
+        s = self.artist_table.select().where(where_clause) #.limit(1)
         # Since there is a unique index on artist_name dont need to limit
 
         rs = s.execute()
@@ -100,13 +122,15 @@ class DbTask(object):
 
 
     def set_tag_pct(self, artist_id, total_tag_count):
-        artist_to_tags_table = al.Table('artist_to_tags', self.metadata, autoload=True)
-        stmt = artist_to_tags_table.update().where(artist_to_tags_table.c.artist_id==artist_id).values(tag_count_pct = artist_to_tags_table.c.tag_count / total_tag_count )
+        # update the tag percentage to the tag count / the total tag count
+        stmt = self.artist_to_tags_table.update().where(artist_to_tags_table.c.artist_id==artist_id).values(tag_count_pct = artist_to_tags_table.c.tag_count / total_tag_count )
 
         stmt.execute()
 
 
     def get_relevant_tags_by_artist(self):
+        # select from DB the tags-artist relations and their names
+        # for the sake of space... only show tags that represent at least 10 % of the whole
         raw_sql = '''
         SELECT  
             a.artist_name, t.tag_name, att.tag_count_pct  
@@ -119,11 +143,16 @@ class DbTask(object):
         ORDER BY  artist_name
         '''
 
+        # execute above statement and return results
         db_results =  self.engine.execute(raw_sql)
-
         return db_results
 
     def get_related_tags(self):
+        #  dont just show me the number of times both tags show up together (count(*))
+        #  show me the sum of all of the pct_counts for each time these appear...
+        #  this means that if two tags that are very relevant to the artist appear x times together
+        #  the score will be higher than if two tags appeared x times but were less relevant to that artist
+
         raw_sql = '''
         SELECT 
             t1.tag_name as t1, t2.tag_name as t2, x.score
@@ -144,19 +173,12 @@ class DbTask(object):
             ON x.tag_id_1 = t1.tag_id
         INNER JOIN tags t2
             ON x.tag_id_2 = t2.tag_id
-        ORDER BY x.score ASC
+        ORDER BY x.score DESC
         '''
 
-        # get the results of above query
+        # get the results of above query and return it
         db_results =  self.engine.execute(raw_sql)
-
         return db_results
-
-
-
-if __name__ == "__main__":
-    t = DbTask()
-    t.get_related_tags()
 
 
 
--- last_fm/last_fm.py
+++ last_fm/last_fm.py
@@ -8,11 +8,12 @@ import numpy as np
 
 class LastFM(DbTask):
     def __init__(self):
+        # define my api credentials
         self.API_URL = "http://ws.audioscrobbler.com/2.0/"
         self.API_KEY = "834ca7240545d8fa587d43662c252773"
         # self.SECRET =  "44195d2012dd899f51af02c7861aad21" 
 
-    # Define all of the methods that I inherit from DB task
+    ## Define all of the methods that I inherit from DB task ##
     def add_artist(self,artist_name):
         super(LastFM, self).add_artist(artist_name)
 
@@ -44,24 +45,27 @@ class LastFM(DbTask):
     def api_request(self, **kwargs):
       json_response = {}
 
-      ## Attribtues that are shared with all API requests
+      ## update the request obect with attribtues 
+      ## that are shared with all API requests
       kwargs.update({
           "api_key":  self.API_KEY,
           "format": "json",
           "limit": 1000
       })
       
-      ## Encode the request into a
+      ## Encode the request into a URL
       url = self.API_URL + "?" + urllib.urlencode(kwargs)
 
       
       try:
           # Send Request and Collect it
           data = urllib2.urlopen( url )
+          # decode json
           json_response = json.load ( data )
 
           data.close()
 
+      # provive URL and HTTP exceptions for API requests
       except urllib2.HTTPError, e:
           print "HTTP error: %d" % e.code
       except urllib2.URLError, e:
@@ -69,63 +73,97 @@ class LastFM(DbTask):
 
       return json_response
 
-    def get_tags_by_artist (self, artist_name, **kwargs):
-        kwargs.update({
-            "method": "artist.gettoptags",
-            "artist":   artist_name,
-        })
-
-        response_data = self.api_request(**kwargs)
-
-        tags = response_data['toptags']['tag']
-        
-        total_tag_count = 0
-
-        for tag in tags:
-            tag_name = tag['name'].encode('utf8')
-            tag_count = tag['count']
-            total_tag_count = total_tag_count + int(tag_count)
-
-            try:
-                self.add_tag(tag_name)
-
-                artist_id = self.get_artist_by_name(artist_name) 
-                tag_id = self.get_tag_by_name(tag_name)
-
-                self.add_artist_to_tag(artist_id, tag_id, tag_count)
-
-                print tag_name
-            except StatementError:
-                pass
-
-        self.set_tag_pct(artist_id, total_tag_count)
-
 
     def get_artist_by_genre(self, genre, **kwargs):
+        # prepare the request object with the genre and the api method
         kwargs.update({
             "method": "tag.gettopartists",
             "tag":    genre,
         })
 
+        # send request to api and store the response
         response_data = self.api_request(**kwargs)
 
+        # access the artists dictionary from the response
         artists =  response_data['topartists']['artist']
         for artist in artists:
+
+            # encode the artist name
             artist_name =  artist['name'].encode('utf8')
+
+            # add this artists into the database (see db.py)
             self.add_artist(artist_name)
+
+            # print out the artist name for debugging  
             print artist_name + '\n' +'=' * 50
+
+            # find relevant tags for the artist and save those to the DB as well
             self.get_tags_by_artist(artist_name)
+
+    def get_tags_by_artist (self, artist_name, **kwargs):
+        # set the additioal parameters for this particulare API request
+        kwargs.update({
+            "method": "artist.gettoptags",
+            "artist":   artist_name,
+        })
+
+        # get data from api using the api_request method (above)
+        response_data = self.api_request(**kwargs)
+
+        # access just the tags data from the response
+        tags = response_data['toptags']['tag']
+        
+        # prepare to find the total count of tags for each artist in order to
+        # determine the relevance each tag is to the artist
+        total_tag_count = 0
+
+        for tag in tags:
+            # get the encoded tag name and tag count for each result
+            tag_name = tag['name'].encode('utf8')
+            tag_count = tag['count']
+
+            # for each iterationn, add the tag count to the total (  )
+            total_tag_count = total_tag_count + int(tag_count)
+
+            try:
+                # add the tag to the DB
+                self.add_tag(tag_name)
+
+                # find the artist and tag id (see db.py)
+
+                artist_id = self.get_artist_by_name(artist_name) 
+                tag_id = self.get_tag_by_name(tag_name)
+
+                # add the artist_id, tag_id relation and the tag count (see db.yp)
+                self.add_artist_to_tag(artist_id, tag_id, tag_count)
+
+                print tag_name # for debugging
+
+            # this try/except block deals with the issue in which a tag is BLANK
+            # i consider this bad data, but regardless this issue had to be dealt with
+            except StatementError:
+                pass
+
+        # update the tag percentage column for each tag
+        self.set_tag_pct(artist_id, total_tag_count)
+
 
     def output_relevant_tags_by_artist(self):
         # open the output file
         f = open('tags_for_artists.txt', 'w+')
+
+        # write the header
         f.write('artist_name, tag_name, tag_count_pct\n')
 
+        # get tags by artists from the databbase
         db_results = self.get_relevant_tags_by_artist()
 
         for r in db_results:
+
+            # for each result add the artist and the tag to the output file
             f.write(r.artist_name + ',' + r.tag_name + ',' + str(r.tag_count_pct) + '\n')
 
+        # close the file
         f.close()
 
 
@@ -182,6 +220,5 @@ def main():
     ### OUTPUT THE FINAL RESULTS
     last_task.output_related_tags()
     
-
 
 if __name__ == "__main__": main()
